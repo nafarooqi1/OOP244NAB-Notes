776759600e05c66d2f06b79e36d020964c8aeada
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2021-01-26 09:54:29 -0500

    cstring.h

---
+++ NAA/05-Jan26/prg.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "cstring.h"
+using namespace std;
+using namespace sdds;
+int main() {
+   char name[20];
+   strCpy(name, "Fardad");
+   cout << name << endl;
+   return 0;
+}
